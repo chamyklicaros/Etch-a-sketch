d72403aae1f8d91602a84724eaeb65d8cce5d5e2
Eudesto <chamyklicaros11@gmail.com>
2026-02-23 03:48:42 +0800

    added access to grid-item in js

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ for (let i = 0; i < maxGridRowSize * maxgridColumnSize; i++) {
     let gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     gridContainer.appendChild(gridItem);
+
+
 }
+
+let gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach(item => {
+    console.log(item); 
+    item.style.backgroundColor = 'lightblue';
+});
+
+
 
