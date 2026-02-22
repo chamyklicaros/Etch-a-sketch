95fb93766a8888dd27e0d402b319470fe5b7d146
Eudesto <chamyklicaros11@gmail.com>
2026-02-23 04:04:55 +0800

    added hover to change color of the grid-items

--- script.js
+++ script.js
@@ -7,17 +7,11 @@ let gridContainer = document.querySelector('#grid-container');
 for (let i = 0; i < maxGridRowSize * maxgridColumnSize; i++) {
     let gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
+    gridItem.style.width = `${100 / maxgridColumnSize}%`;
+    gridItem.style.height = `${100 / maxGridRowSize}%`;
+    gridItem.addEventListener('mouseover', function() {
+        gridItem.style.backgroundColor = '#005459';
+    })
+   
     gridContainer.appendChild(gridItem);
-
-
 }
-
-let gridItems = document.querySelectorAll('.grid-item');
-
-gridItems.forEach(item => {
-    console.log(item); 
-    item.style.backgroundColor = 'lightblue';
-});
-
-
-
