0cb727ea43b51b23aefb29e7914fb1f011db615b
Eudesto <chamyklicaros11@gmail.com>
2026-02-23 07:24:36 +0800

    added size changes

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let maxGridRowSize = 16
-let maxgridColumnSize = maxGridRowSize
-
 let gridContainer = document.querySelector('#grid-container');
 
-
-for (let i = 0; i < maxGridRowSize * maxgridColumnSize; i++) {
+function buildGrid(size) {
+    gridContainer.innerHTML = '';
+for (let i = 0; i < size * size; i++) {
+    
     let gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
-    gridItem.style.width = `${100 / maxgridColumnSize}%`;
-    gridItem.style.height = `${100 / maxGridRowSize}%`;
+    gridItem.style.width = `${100 / size}%`;
+    gridItem.style.height = `${100 / size}%`;
     gridItem.addEventListener('mouseover', function() {
         gridItem.style.backgroundColor = '#005459';
     })
    
     gridContainer.appendChild(gridItem);
 }
+}
+
+let gridSize = document.querySelector('#grid-size');
+let inputBtn = document.querySelector('#input-button');
+
+inputBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  let size = parseInt(gridSize.value);
+  if (size > 0 && size <= 100) buildGrid(size);
+});
+
+buildGrid(parseInt(gridSize.value));
